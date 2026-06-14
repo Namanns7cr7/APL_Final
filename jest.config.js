32fd9d8af064881c3973a675d71e713444bb3a77
Namanns7cr7 <namansharma2664@gmail.com>
2026-06-14 19:56:04 +0530

    shipmate(blocker): No test coverage — jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+export default {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
